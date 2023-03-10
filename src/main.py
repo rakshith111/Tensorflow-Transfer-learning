b5951f215afddcd4b612716f1201069704d171bf
rakshith111 <rakshithbabu111@gmail.com>
2023-03-11 00:00:52 +0530

    Add dataset config paths

--- src/main.py
+++ src/main.py
@@ -12,5 +12,12 @@ sys.path.insert(0, parentdir)
 from libs._base_logger import logger
 from libs.common import check_gpu
 if __name__ == "__main__":
+    if not os.path.exists("src/dataset/train") and not os.path.exists("src/dataset/test"):
+        os.makedirs("src/dataset/train")
+        os.makedirs("src/dataset/test")
+        logger.info("Created dataset directory")
+        logger.info("Add the dataset to the dataset directory Class name should be the same as the folder name")
+        exit()
+       
     logger.info(f"Tensorflow version: {tf.__version__}")
     logger.info(f"Gpu available: {check_gpu()}")
